418b8d69560a01eaa7439dd2fd41a68ab8ecda9b
lixiao <1135908647@qq.ocm>
2023-08-18 14:37:42 +0800

    2023/8/18 构建公司电脑本地基础版本

--- jiangshan/src/main/java/com/example/jiangshan/config/JSAPI.java
+++ jiangshan/src/main/java/com/example/jiangshan/config/JSAPI.java
@@ -5,7 +5,7 @@ import com.wechat.pay.java.core.RSAAutoCertificateConfig;
 
 public class JSAPI {
     public static final String merchantId = "1636296954";
-    public static final String privateKeyPath = "E:\\龙岩\\privateKey\\apiclient_key.pem";
+    public static final String privateKeyPath = "D:\\privateKey\\apiclient_key.pem";
     public static final String merchantSerialNumber = "2DBF0F8C1DDFF43780020628E81049536094D197";
     public static final String apiV3key = "sd15as1d56as1d56a156d156ad156as1";
 
