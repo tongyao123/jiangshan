fbe0c4aca94a0dc82fd0b4c9e71c71e3ca0a763c
lixiao <1135908647@qq.ocm>
2023-08-28 18:23:52 +0800

    2023/8/28 公司电脑更新农业产业

--- jiangshan/src/main/java/com/example/jiangshan/function/NumericalConversion.java
+++ jiangshan/src/main/java/com/example/jiangshan/function/NumericalConversion.java
@@ -2,8 +2,13 @@ package com.example.jiangshan.function;
 
 public class NumericalConversion {
     public static Integer LongToInteger(Object numrice) {
+
+        if (numrice.equals("") || numrice==null || numrice.equals(0)){
+            return 0;
+        }
         if (numrice instanceof Double) {
             Double d = (double) numrice;
+
             return d.intValue();
         } else {
             return Integer.parseInt(numrice.toString());
